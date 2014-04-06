de8617a2f48cf300355f969c5eb43cce21341996
Alexander Pope <alex@pope-industries.com>
2014-04-06 16:45:02 +0200

    add sync/async interface with Promises

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var path = require('path')
+var Promise = require('bluebird')
+	, path = require('path')
 	, fs = require('fs')
+	, readFileAsync = Promise.promisify(fs.readFile)
+	, readFileSync = fs.readFileSync
 	, uglify = require('uglify-js')
 	, csso = require('csso')
 
@@ -9,41 +12,106 @@ var path = require('path')
 	, RE_HREF = /href=["|'](.+)["|']/;
 
 /**
- * Parse 'html' for <script> and <link> tags containing an 'inline' attribute
+ * Parse 'html' for <script> and <link> tags containing an 'inline' attribute,
+ * and replace with compressed file contents
  * @param {String} htmlpath
  * @param {String} html
+ * @param {Function} [fn(err, html)]
+ * @returns {Promise}
  */
-module.exports = function (htmlpath, html) {
-	var match;
-
+module.exports = function (htmlpath, html, fn) {
 	// Remove file name if necessary
 	htmlpath = path.extname(htmlpath).length ? path.dirname(htmlpath) : htmlpath;
 
-	// Parse inline <script> tags
-	while (match = RE_INLINE_SOURCE.exec(html)) {
-		html = inline('js', match[1], htmlpath, html);
-	}
+	// Parse inline sources
+	var sources = parse(html);
 
-	// Parse inline <link> tags
-	while (match = RE_INLINE_HREF.exec(html)) {
-		html = inline('css', match[1], htmlpath, html);
+	if (sources.length) {
+		// Get inlined content
+		return Promise.all(sources.map(function (source) {
+			return getContent(source.type, source.content, htmlpath, true)
+				.then(function (content) {
+					return [source.content, content];
+				});
+		// Replace inlined content in html
+		})).then(function (sources) {
+			sources.forEach(function (source) {
+				html = html.replace(source[0], source[1]);
+			});
+			return html;
+		}).nodeify(fn);
+
+	// Return untouched html if no content to inline
+	} else {
+		return Promise.resolve(html).nodeify(fn);
+	}
+};
+
+/**
+ * Synchronously parse 'html' for <script> and <link> tags containing an 'inline' attribute,
+ * and replace with compressed file contents
+ * @param {String} htmlpath
+ * @param {String} html
+ * @returns {String}
+ */
+module.exports.sync = function (htmlpath, html) {
+	// Remove file name if necessary
+	htmlpath = path.extname(htmlpath).length ? path.dirname(htmlpath) : htmlpath;
+
+	// Parse inline sources
+	var sources = parse(html);
+
+	if (sources.length) {
+		sources.map(function (source) {
+			try {
+				var content = getContent(source.type, source.content, htmlpath, false);
+				return [source.content, content];
+			} catch (err) {
+				// Remove 'inline' attribute if error loading content
+				return [source.content, source.content.replace(' inline', '')];
+			}
+		// Replace inlined content in html
+		}).forEach(function (source) {
+			html = html.replace(source[0], source[1]);
+		})
 	}
 
 	return html;
 };
 
 /**
- * Inline a 'source' tag in 'html'
+ * Parse 'html' for inlineable sources
+ * @param {String} html
+ * @returns {Array}
+ */
+function parse (html) {
+	var sources = []
+		, match;
+
+	// Parse inline <script> tags
+	while (match = RE_INLINE_SOURCE.exec(html)) {
+		sources.push({content: match[1], type: 'js'});
+	}
+
+	// Parse inline <link> tags
+	while (match = RE_INLINE_HREF.exec(html)) {
+		sources.push({content: match[1], type: 'css'});
+	}
+
+	return sources;
+}
+
+/**
+ * Retrieve content for 'source'
  * @param {String} type
  * @param {String} source
  * @param {String} htmlpath
- * @param {String} html
+ * @param {Boolean} async
  * @returns {String}
  */
-function inline (type, source, htmlpath, html) {
+function getContent (type, source, htmlpath, async) {
 	var isCSS = (type == 'css')
 		, tag = isCSS ? 'style' : 'script'
-		, content = '<' + tag + '>'
 		// Parse url
 		, sourcepath = source.match(isCSS ? RE_HREF : RE_SRC)[1]
 		, filepath = sourcepath.indexOf('/') == 0
@@ -51,21 +119,38 @@ function inline (type, source, htmlpath, html) {
 			? path.resolve(process.cwd(), sourcepath.slice(1))
 			// Relative
 			: path.resolve(htmlpath, sourcepath)
-		, filecontent;
+		, content;
 
-	if (fs.existsSync(filepath)) {
-		filecontent = fs.readFileSync(filepath, 'utf8');
-		// Compress
-		try {
-			filecontent = isCSS
-				? csso.justDoIt(filecontent)
-				: uglify.minify(filecontent, {fromString: true}).code;
-		} catch (err) { }
-		content += filecontent + '</' + tag + '>';
-		// Inline
-		return html.replace(source, content);
+	if (async) {
+		return readFileAsync(filepath, 'utf8')
+			.then(function (content) {
+				return '<' + tag + '>'
+					+ compressContent(type, content)
+					+ '</' + tag + '>';
+			}).catch(function (err) {
+				// Remove 'inline' attribute if error loading content
+				return source.replace(' inline', '');
+			});
 	} else {
-		// Remove 'inline' attribute
-		return html.replace(source, source.replace(' inline', ''))
+		content = readFileSync(filepath, 'utf8');
+		return '<' + tag + '>'
+			+ compressContent(type, content)
+			+ '</' + tag + '>';
 	}
+}
+
+/**
+ * Compress 'content' of 'type'
+ * @param {String} type
+ * @param {String} content
+ * @returns {String}
+ */
+function compressContent (type, content) {
+	try {
+		content = (type == 'css')
+			? csso.justDoIt(content)
+			: uglify.minify(content, {fromString: true}).code;
+	} catch (err) { /* return uncompressed if error */}
+
+	return content;
 }
--- test/inline-test.js
+++ test/inline-test.js
@@ -7,99 +7,162 @@ describe('inline-source', function() {
 	before(function() {
 		process.chdir('./test/fixtures');
 	});
-	describe('script tag inlining', function() {
-		it('should ignore commented sources', function() {
+	describe('<script> tag inlining', function() {
+		it('should ignore commented sources', function(done) {
 			var test = '<!-- <script inline src="foo.js"></script> -->';
-			var html = inline(process.cwd(), test);
-			html.should.eql(test);
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql(test);
+				done();
+			});
 		});
-		it('should ignore sources that don\'t contain an "inline" attribute', function() {
+		it('should ignore sources that don\'t contain an "inline" attribute', function(done) {
 			var test = '<script src="foo.js"></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql(test);
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql(test);
+				done();
+			});
 		});
-		it('should ignore sources that don\'t contain an "inline" attribute but contain the string "inline"', function() {
+		it('should ignore sources that don\'t contain an "inline" attribute but contain the string "inline"', function(done) {
 			var test = '<script src="inline.js"></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql(test);
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql(test);
+				done();
+			});
 		});
-		it('should inline sources that contain an "inline" attribute', function() {
+		it('should inline sources that contain an "inline" attribute', function(done) {
 			var test = '<script inline src="foo.js"></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<script>var foo=this;</script>');
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<script>var foo=this;</script>');
+				done();
+			});
 		});
-		it('should inline sources that contain an "inline" attribute on a line with leading whitespace', function() {
+		it('should inline multiple sources that contain an "inline" attribute', function(done) {
+			var test = '<script inline src="foo.js"></script>\n<script inline src="bar.js"></script>';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<script>var foo=this;</script>\n<script>var bar=this;</script>');
+				done();
+			});
+		});
+		it('should expose a Promise interface in addition to callback', function(done) {
+			var test = '<script inline src="foo.js"></script>';
+			inline(process.cwd(), test).then(function (html) {
+				html.should.eql('<script>var foo=this;</script>');
+				done();
+			});
+		});
+		it('should inline sources that contain an "inline" attribute on a line with leading whitespace', function(done) {
 			var test = '		<script inline src="foo.js"></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql('		<script>var foo=this;</script>');
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('		<script>var foo=this;</script>');
+				done();
+			});
 		});
-		it('should inline sources that contain an "inline" attribute at the end of the <script> tag', function() {
+		it('should inline sources that contain an "inline" attribute at the end of the <script> tag', function(done) {
 			var test = '<script src="foo.js" inline></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<script>var foo=this;</script>');
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<script>var foo=this;</script>');
+				done();
+			});
 		});
-		it('should inline sources that contain an "inline" attribute at the end of the <script> tag surrounded by whitespace', function() {
+		it('should inline sources that contain an "inline" attribute at the end of the <script> tag surrounded by whitespace', function(done) {
 			var test = '<script src="foo.js" inline ></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<script>var foo=this;</script>');
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<script>var foo=this;</script>');
+				done();
+			});
 		});
-		it('should remove the "inline" attribute for sources that can\'t be found', function() {
-			var test = '<script inline src="bar.js"></script>';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<script src="bar.js"></script>');
+		it('should remove the "inline" attribute for sources that can\'t be found', function(done) {
+			var test = '<script inline src="baz.js"></script>\n<script inline src="foo.js"></script>';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<script src="baz.js"></script>\n<script>var foo=this;</script>');
+				done();
+			});
 		});
-		it('should inline sources referenced by relative path', function() {
+		it('should inline sources referenced by relative path', function(done) {
 			var test = '<script inline src="./nested/foo.js"></script>';
-			var html = inline(path.resolve('index.html'), test);
-			html.should.eql('<script>var foo=this;</script>');
+			inline(path.resolve('index.html'), test, function (err, html) {
+				html.should.eql('<script>var foo=this;</script>');
+				done();
+			});
 		});
-		it('should inline sources referenced by absolute path', function() {
+		it('should inline sources referenced by absolute path', function(done) {
 			var test = '<script inline src="/nested/foo.js"></script>';
-			var html = inline(path.resolve('nested/index.html'), test);
-			html.should.eql('<script>var foo=this;</script>');
+			inline(path.resolve('nested/index.html'), test, function (err, html) {
+				html.should.eql('<script>var foo=this;</script>');
+				done();
+			});
 		});
 	});
 
-	describe('link tag inlining', function() {
-		it('should ignore commented sources', function() {
+	describe('<link> tag inlining', function() {
+		it('should ignore commented sources', function(done) {
 			var test = '<!-- <link inline rel="stylesheet" href="foo.css"> -->';
-			var html = inline(process.cwd(), test);
-			html.should.eql(test);
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql(test);
+				done();
+			});
 		});
-		it('should ignore sources that don\'t contain an "inline" attribute', function() {
+		it('should ignore sources that don\'t contain an "inline" attribute', function(done) {
 			var test = '<link rel="stylesheet" href="foo.js">';
-			var html = inline(process.cwd(), test);
-			html.should.eql(test);
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql(test);
+				done();
+			});
 		});
-		it('should ignore sources that don\'t contain an "inline" attribute but contain the string "inline"', function() {
+		it('should ignore sources that don\'t contain an "inline" attribute but contain the string "inline"', function(done) {
 			var test = '<link rel="stylesheet" href="inline.js">';
-			var html = inline(process.cwd(), test);
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql(test);
+				done();
+			});
+		});
+		it('should inline sources that contain an "inline" attribute', function(done) {
+			var test = '<link inline rel="stylesheet" href="foo.css">';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<style>body{background-color:#fff}</style>');
+				done();
+			});
+		});
+		it('should inline sources that contain an "inline" attribute on a line with leading whitespace', function(done) {
+			var test = '		<link inline rel="stylesheet" href="foo.css">';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('		<style>body{background-color:#fff}</style>');
+				done();
+			});
+		});
+		it('should inline sources that contain an "inline" attribute at the end of the <link> tag', function(done) {
+			var test = '<link rel="stylesheet" href="foo.css" inline>';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<style>body{background-color:#fff}</style>');
+				done();
+			});
+		});
+		it('should inline sources that contain an "inline" attribute at the end of the <link> tag surrounded by whitespace', function(done) {
+			var test = '<link rel="stylesheet" href="foo.css" inline >';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<style>body{background-color:#fff}</style>');
+				done();
+			});
+		});
+		it('should remove the "inline" attribute for sources that can\'t be found', function(done) {
+			var test = '<link inline rel="stylesheet" href="bar.css">';
+			inline(process.cwd(), test, function (err, html) {
+				html.should.eql('<link rel="stylesheet" href="bar.css">');
+				done();
+			});
+		});
+	});
+
+	describe('sync() inlining', function () {
+		it('should synchronously ignore sources that don\'t contain an "inline" attribute', function() {
+			var test = '<script src="foo.js"></script>';
+			var html = inline.sync(process.cwd(), test);
 			html.should.eql(test);
 		});
-		it('should inline sources that contain an "inline" attribute', function() {
-			var test = '<link inline rel="stylesheet" href="foo.css">';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<style>body{background-color:#fff}</style>');
-		});
-		it('should inline sources that contain an "inline" attribute on a line with leading whitespace', function() {
-			var test = '		<link inline rel="stylesheet" href="foo.css">';
-			var html = inline(process.cwd(), test);
-			html.should.eql('		<style>body{background-color:#fff}</style>');
-		});
-		it('should inline sources that contain an "inline" attribute at the end of the <link> tag', function() {
-			var test = '<link rel="stylesheet" href="foo.css" inline>';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<style>body{background-color:#fff}</style>');
-		});
-		it('should inline sources that contain an "inline" attribute at the end of the <link> tag surrounded by whitespace', function() {
-			var test = '<link rel="stylesheet" href="foo.css" inline >';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<style>body{background-color:#fff}</style>');
-		});
-		it('should remove the "inline" attribute for sources that can\'t be found', function() {
-			var test = '<link inline rel="stylesheet" href="bar.css">';
-			var html = inline(process.cwd(), test);
-			html.should.eql('<link rel="stylesheet" href="bar.css">');
+		it('should synchronously inline sources that contain an "inline" attribute', function() {
+			var test = '<script inline src="foo.js"></script>';
+			var html = inline.sync(process.cwd(), test);
+			html.should.eql('<script>var foo=this;</script>');
 		});
 	});
 });
