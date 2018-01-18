a7e40f2578533fd6242c7819408dc78373d2451d
taoqf <tao_qiufeng@126.com>
2018-01-18 16:52:42 +0800

    fix typedefinition (#64)
    * add type definition
    
    * export inline as a module

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,18 @@
-interface Options {
-  attribute: string | boolean;
-  compress: boolean;
-  fs: any; // like MemoryFileSystem
-  handlers: [({}, {}) => Promise<any>];
-  ignore: string[] | { [key: string]: string };
-  pretty: boolean;
-  rootpath: string;
-  saveRemote: boolean;
-  svgAsImage: boolean;
-  swallowErrors: boolean;
+declare namespace inline {
+	interface Options {
+		attribute: string | boolean;
+		compress: boolean;
+		fs: any; // like MemoryFileSystem
+		handlers: [(source: any, context: any) => Promise<any>];
+		ignore: string[] | { [key: string]: string };
+		pretty: boolean;
+		rootpath: string;
+		saveRemote: boolean;
+		svgAsImage: boolean;
+		swallowErrors: boolean;
+	}
 }
 
-declare function inline(htmlpath: string, options: Partial<Options>): Promise<string>;
+declare function inline(htmlpath: string, options?: Partial<inline.Options>): Promise<string>;
 
 export = inline;
